787d4b76adaeebe6958828f5aa501d606fc1a7d9
Elias Hogstvedt <eliashogstvedt@gmail.com>
2025-10-18 07:01:53 +0200

    fix example

--- examples/udp_client_server.lua
+++ examples/udp_client_server.lua
@@ -1,12 +1,12 @@
 local socket = require("ljsocket")
 local port = 8080
-local address = socket.find_first_address("*", port)
 
 do -- server
+    local bind_address = socket.find_first_address("*", port)  -- 0.0.0.0 for binding
     local server = assert(socket.create("inet", "dgram", "udp"))
     assert(server:set_blocking(false))
-    assert(server:bind(address))
-    print("hosting at ", address:get_ip(), address:get_port())
+    assert(server:bind(bind_address))
+    print("hosting at ", bind_address:get_ip(), bind_address:get_port())
 
     function update_server()
         local data, addr = server:receive_from()
@@ -21,17 +21,18 @@ do -- server
 end
 
 do -- client
+    local send_address = socket.find_first_address("127.0.0.1", port)  -- localhost for sending
     local client = assert(socket.create("inet", "dgram", "udp"))
     assert(client:set_blocking(false))
     local next_send = 0
 
     function update_client()
         if next_send < os.clock() then
-            assert(client:send_to(address, "hello from client " .. os.clock()))
+            assert(client:send_to(send_address, "hello from client " .. os.clock()))
             next_send = os.clock() + math.random() + 0.5
         end
 
-        local data, addr = client:receive_from(address)
+        local data, addr = client:receive_from()
 
         if data then
             print(data, addr:get_ip(), addr:get_port())
