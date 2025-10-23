1bcb766052ac15095c011ad427a190ab44c13b65
Elias Hogstvedt <eliashogstvedt@gmail.com>
2025-10-23 10:52:02 +0200

    use assert on socket functions

--- test/tcp_client_server.lua
+++ test/tcp_client_server.lua
@@ -7,21 +7,13 @@ test('TCP client-server communication', function()
 	local update_client
 
 	-- Server setup
-	local info = socket.find_first_address_info("*", port, {"passive"}, "inet", "stream", "tcp")
-	ok(info ~= nil, "should find address info for server")
+	local info = assert(socket.find_first_address_info("*", port, {"passive"}, "inet", "stream", "tcp"))
+	local server = assert(socket.create(info.family, info.socket_type, info.protocol))
 
-	local server = socket.create(info.family, info.socket_type, info.protocol)
-	ok(server ~= nil, "should create server socket")
-
-	server:set_option("reuseaddr", 1)
-	local blocking_set = server:set_blocking(false)
-	ok(blocking_set, "should set server to non-blocking")
-
-	local bound = server:bind(info)
-	ok(bound, "should bind server to port")
-
-	local listening = server:listen()
-	ok(listening, "should start listening")
+	assert(server:set_option("reuseaddr", 1))
+	assert(server:set_blocking(false))
+	assert(server:bind(info))
+	assert(server:listen())
 
 	local current_client = nil
 
@@ -30,8 +22,7 @@ test('TCP client-server communication', function()
 
 		if client then
 			current_client = client
-			local blocking_set = client:set_blocking(false)
-			ok(blocking_set, "should set client socket to non-blocking")
+			assert(client:set_blocking(false))
 		elseif err ~= "timeout" then
 			ok(false, "server accept error: " .. tostring(err))
 		end
@@ -40,45 +31,37 @@ test('TCP client-server communication', function()
 			local str, err = current_client:receive()
 			if str then
 				ok(str == "hello", "server should receive 'hello' message")
-				local sent = current_client:send(str)
-				ok(sent, "server should send echo message")
+				assert(current_client:send(str))
 			elseif err == "closed" then
-				local closed = current_client:close()
-				ok(closed, "should close client connection")
+				assert(current_client:close())
 				current_client = nil
 				return true
 			elseif err ~= "timeout" then
-				ok(false, "server receive error: " .. tostring(err))
+				error("server receive error: " .. tostring(err))
 			end
 		end
 	end
 
 	-- Client setup
-	local client = socket.create("inet", "stream", "tcp")
-	ok(client ~= nil, "should create client socket")
-
-	local connected = client:connect("localhost", port)
-	ok(connected, "client should connect to server")
-
-	local blocking_set = client:set_blocking(false)
-	ok(blocking_set, "should set client to non-blocking")
+	local client = assert(socket.create("inet", "stream", "tcp"))
+	assert(client:connect("localhost", port))
+	assert(client:set_blocking(false))
 
 	local sent_message = false
 
 	function update_client()
 		if client:is_connected() then
 			if not sent_message then
-				local sent = client:send("hello")
-				ok(sent, "client should send 'hello' message")
+				assert(client:send("hello"))
 				sent_message = true
 			else
 				local str, err = client:receive()
 
 				if str then
 					ok(str == "hello", "client should receive echo 'hello'")
-					client:close()
+					assert(client:close())
 				elseif err ~= "timeout" then
-					ok(false, "client receive error: " .. tostring(err))
+					error("client receive error: " .. tostring(err))
 				end
 			end
 		end
